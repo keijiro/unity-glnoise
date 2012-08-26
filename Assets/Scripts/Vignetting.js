761e016e4b1d6da14583aaeb8a7f1ff0f9bc7052
keijiro <keijiro@gmail.com>
2012-08-26 21:37:03 +0900

    Added vignette FX.

---
+++ Assets/Scripts/Vignetting.js
@@ -0,0 +1,37 @@
+#pragma strict
+
+@script ExecuteInEditMode
+@script RequireComponent(Camera)
+
+var intensity = 0.375;
+var blur = 0.1;
+var blurSpread = 1.5;
+
+var vignetteMaterial : Material;
+var separableBlurMaterial : Material;	
+
+function OnRenderImage(source : RenderTexture, destination : RenderTexture) {	
+	var widthOverHeight = (1.0 * source.width) / source.height;
+	var oneOverBaseSize = 1.0 / 512;
+
+	var halfRezColor = RenderTexture.GetTemporary(source.width / 2, source.height / 2, 0);
+	var quarterRezColor = RenderTexture.GetTemporary(source.width / 4, source.height / 4, 0);
+	var secondQuarterRezColor = RenderTexture.GetTemporary(source.width / 4, source.height / 4, 0);
+
+	Graphics.Blit(source, halfRezColor);
+	Graphics.Blit(halfRezColor, quarterRezColor);
+
+	separableBlurMaterial.SetVector("offsets", Vector4(0.0, blurSpread * oneOverBaseSize, 0.0, 0.0));
+	Graphics.Blit(quarterRezColor, secondQuarterRezColor, separableBlurMaterial);
+	separableBlurMaterial.SetVector("offsets", Vector4(blurSpread * oneOverBaseSize / widthOverHeight, 0.0, 0.0, 0.0));
+	Graphics.Blit(secondQuarterRezColor, quarterRezColor, separableBlurMaterial);
+
+	vignetteMaterial.SetFloat("_Intensity", intensity);
+	vignetteMaterial.SetFloat("_Blur", blur);
+	vignetteMaterial.SetTexture("_VignetteTex", quarterRezColor);
+	Graphics.Blit(source, destination, vignetteMaterial);
+	
+	RenderTexture.ReleaseTemporary(halfRezColor);			
+	RenderTexture.ReleaseTemporary(quarterRezColor);	
+	RenderTexture.ReleaseTemporary(secondQuarterRezColor);	
+}
